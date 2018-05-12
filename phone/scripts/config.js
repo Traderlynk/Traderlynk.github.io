515b8bd16bf4837f135f7d83ed6364f6f047d252
Dele Olajide <dele@4ng.net>
2018-05-12 17:00:13 +0100

    wip

--- phone/scripts/config.js
+++ phone/scripts/config.js
@@ -1,7 +1,7 @@
 var user = {
     Destination : "3366",
-    User        : "default",
-    Pass        : "default",
+    User        : "1001",
+    Pass        : "1234",
     Realm       : "212.147.112.162",
     Display     : "Guest",
     WSServer    : "wss://traderlynk.4ng.net/sip/proxy?url=ws://212.147.112.162:5066"
